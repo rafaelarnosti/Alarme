523def92c30c8d9e5d947ab5d991cc00a96c9ebe
RafaelArnosti <rlm.arnosti@gmail.com>
2017-06-12 19:49:48 -0300

    Commit

--- app/src/main/java/com/rafaelarnosti/alarme/AlarmeReceiver.java
+++ app/src/main/java/com/rafaelarnosti/alarme/AlarmeReceiver.java
@@ -1,10 +1,26 @@
 package com.rafaelarnosti.alarme;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
 /**
  * Created by logonrm on 12/06/2017.
  */
 
-public class AlarmeReceiver {
+public class AlarmeReceiver extends BroadcastReceiver {
 
-    
+
+
+    private MediaPlayer mp = null;
+    public AlarmeReceiver() {
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        mp= MediaPlayer.create(context, R.raw.metalgearsolid);
+        mp.start();
+        Toast.makeText(context, "Alarm....", Toast.LENGTH_LONG).show();
+    }
 }
